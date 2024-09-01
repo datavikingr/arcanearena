96f2f12b0531b281c867668a4631e83727164904
Alex Haskins <alexanderwantshislibraryback@gmail.com>
2024-09-01 17:25:09 -0400

    standardized node structure; player is CB2D, applies physics forces. using purp for testing.

--- scripts/bluewiz_rigid.gd
+++ scripts/bluewiz_rigid.gd
@@ -1,23 +1,26 @@
-extends CharacterBody2D
+extends RigidBody2D
 
-@export var speed: int = 200 # Feels like a good lateral speed for now.
+@export var speed: int  = 200 # Self evident & pretty quick, too
 @export var jump_speed: int = 300 # 400 is a little too high for the maps. 300 feels good.
-@export var push_force: float = 200.0 # This represents the player's inertia.
 @onready var sprite = get_node("Sprite")
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") # gravity = 980
 var is_holding = false
 var left_right: float = 0
+var is_grounded: bool = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass 
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta) -> void: 
+func _on_body_entered(_body) -> void:
+	rotation = 0
+	angular_velocity = 0 # No really, don't bloody rotate unless the player says so
+	$GroundDetector.enabled = true
+
+func _physics_process(delta) -> void:
+	rotation = 0
 	player_movement(delta)
 	if Input.is_action_just_pressed("blue_jump"):
 		player_jump(delta)
-	physics_collisions()
 	if Input.is_action_just_pressed("blue_attack"):
 		player_attack(delta)
 	if Input.is_action_just_pressed("blue_special"):
@@ -25,40 +28,39 @@ func _physics_process(delta) -> void:
 	if Input.is_action_just_pressed("blue_block"):
 		player_block(delta)
 
-func player_movement(delta) -> void:
-	velocity.y += gravity * delta
-	left_right = Input.get_axis("blue_left", "blue_right") # Move direction
+func player_movement(_delta) -> void:
+	#linear_velocity.y += gravity * delta # downward force
+	left_right = Input.get_axis("blue_left", "blue_right")# Move direction
 	#Perform filter for arcade-like on/off controls
 	if left_right < -0.25: #Pressed Left
 		sprite.flip_h = true # toggles mirror on; faces left
 		#TODO: animation line
-		velocity.x = -1 * speed # Go left
+		linear_velocity.x = -1 * speed # Go left
 	elif left_right > 0.25: #Pressed Right
 		sprite.flip_h = false # toggles mirror off; faces right
 		#TODO: animation line
-		velocity.x = speed
+		linear_velocity.x = speed
 	else: #Standing Still
 		#TODO: animation line for ground idle
 		#TODO: animation line for falling idle
-		velocity.x = 0
-	move_and_slide()
-
-func is_slide() -> bool:
-	return is_on_floor() and Input.is_action_pressed("blue_down")
+		linear_velocity.x = 0
+	#rotation = 0 # But don't let it spin, yet
 
 func player_jump(_delta) -> void:
-	if is_slide(): # Slide
+	if not is_slide(): # Jump
+		#TODO: animation line for jumping
+		linear_velocity.y = -1 * jump_speed #omg Godot defines "Up" as -Y and NOT +Y. *sigh* 
+		print("Jump!")
+	else: # Slide
 		if sprite.flip_h == true:
 			left_right = -1
 		else:
 			left_right = 1
+		#TODO: animation line for sliding
+		linear_velocity.x = left_right * speed * 1.75
 		print("Slide!")
-		velocity.x = left_right * speed * 2
-	else: #Jump
-		velocity.y = -1 * jump_speed #omg Godot defines "Up" as -Y and NOT +Y. *sigh* 
-		print("Jump!")
-	move_and_slide()
-	
+	#TODO: Velocity check for falling state
+
 func player_attack(_delta) -> void:
 	print("Attack!")
 
@@ -68,11 +70,6 @@ func player_special(_delta) -> void:
 func player_block(_delta) -> void:
 	print("Block!")
 
-func physics_collisions() -> void:
-	# after calling move_and_slide()
-	for i in range(get_slide_collision_count()):
-		var c = get_slide_collision(i)
-		if c.get_collider().is_in_group("balls"):
-			print("Ball!")
-			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
-			break
+func is_slide() -> bool:
+	is_grounded = $GroundDetector.is_colliding()
+	return Input.is_action_pressed("blue_down") and is_grounded
