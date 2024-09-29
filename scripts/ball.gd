09939dfcd52bd86df02b5914f1d5f3c54ca3527a
datavikingr <alexanderwantshislibraryback@gmail.com>
2024-09-29 00:01:41 -0400

    removed ball debugging. ball-wormhole fixed, confirmed. Added input mapping.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -11,7 +11,6 @@ func _process(_delta): # Called every frame. 'delta' is the elapsed time since t
 
 func _integrate_forces(state) -> void:
 	velocity_clamp(state)
-	debug_statements(state)
 	pass
 
 func velocity_clamp(state) -> void:
@@ -19,14 +18,3 @@ func velocity_clamp(state) -> void:
 	if velocity.length() > MAX_VELOCITY: # If we're exceeding the speed limit
 		velocity = velocity.normalized() * MAX_VELOCITY # Clamp the velocity magnitude
 	state.set_linear_velocity(velocity) # Hard-write the clamped speed to the object
-
-func debug_statements(state) -> void:
-	for i in range(state.get_contact_count()):
-		print("We have contact.")
-		var collider = state.get_contact_collider_object(i)
-		if collider.is_in_group("players"): # assuming the players are in a "players" group
-			var contact_impulse = state.get_contact_impulse(i)
-			var delta_velocity = state.get_contact_local_normal(i) * contact_impulse
-			print("Player touch detected!")
-			print("Impulse received: ", str(contact_impulse))
-			print("Delta-v: ", str(delta_velocity))
